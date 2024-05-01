8e1b3ea7cea12a19f9e03344464b93d1245251e2
Victor GC <vgarcanuk@gmail.com>
2024-05-01 22:46:20 +0100

    UPDATE: Gif Added to point at the playlist selector.

--- assets/js/jq/jq.js
+++ assets/js/jq/jq.js
@@ -3,6 +3,7 @@
         $('#lists-display').slideToggle(750);
         $('#player-display').slideToggle(750);
         $('#happy-lemon').fadeToggle(1000);
+        $('#arrow-lists').fadeToggle(1000);
     });
 
 })(jQuery);
