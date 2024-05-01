3c00c9d5353508b047a1ba7203865f67d99ec6bd
Victor GC <vgarcanuk@gmail.com>
2024-05-01 14:32:14 +0100

    UPDATE: Uninstalled jquery and CDN imported.

--- assets/js/jq/jq.js
+++ assets/js/jq/jq.js
@@ -1,4 +1,6 @@
-$('.btn').on('click', function() {
-    $('#lists-display').slideToggle(750);
-    $('#player-display').slideToggle(750);
-});
+(function($) {
+    $('.btn').on('click', function() {
+        $('#lists-display').slideToggle(750);
+        $('#player-display').slideToggle(750);
+    });
+})(jQuery);
